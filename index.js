e5a062b37270205083ff013bbacc75bb50357e35
Andrew Bergheim <andrewbergheim@gmail.com>
2020-01-11 04:10:35 -0700

    added questions and prompt with axios call

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const inquirer = require("inquirer")
 const axios = require("axios")
 
 const questions = [
-  
+  {
+    type:"checkbox",
+    name:"color",
+    message:"Which of these colors is your favorite?",
+    choices:["green","blue","pink","red"]
+    },
+  {
+    type:"input",
+    name:"user",
+    message:"What GitHub username would you like to generate a profile for?"
+  }
 ];
+
+inquirer.prompt(questions).then(function({user}){
+    queryURL = "https://api.github.com/users/" + user.name
+    axios.get(queryURL);
+});
+
 
 function writeToFile(fileName, data) {
  
